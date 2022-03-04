6dada200eadab3ebad597691df905521eac7927c
MoniNeo <moni.gangane@gmail.com>
2022-03-04 15:49:13 +0530

    Login user create token using jsonwebtoken hashbcrypt and bcrypt comparesync for compare password

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,9 +1,9 @@
 import express from "express";
 const userRouter = express.Router();
-import {getAlluserFunction, registerFunction} from "../controller/UserController.js"
+import {getAlluserFunction, loginFunction, registerFunction} from "../controller/UserController.js"
 import {upload} from "../middleware/multer.js"
 
 userRouter.route("/registerService").post(upload.single('profile'),registerFunction)
 userRouter.route("/alluserService").get(getAlluserFunction)
-
+userRouter.route("/loginService").post(loginFunction)
 export default userRouter
