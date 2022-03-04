2062380d429e6bb6e5d712d5aeb8c287d382c6bb
MoniNeo <moni.gangane@gmail.com>
2022-03-04 14:42:55 +0530

    get all user registered

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 const userRouter = express.Router();
-import {registerFunction} from "../controller/UserController.js"
+import {getAlluserFunction, registerFunction} from "../controller/UserController.js"
 import {upload} from "../middleware/multer.js"
 
 userRouter.route("/registerService").post(upload.single('profile'),registerFunction)
+userRouter.route("/alluserService").get(getAlluserFunction)
 
 export default userRouter
